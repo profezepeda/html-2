b7f2811794dea04ffbf6e95ec67a9686eb9b6687
Marcelo Zepeda <marcelo@fabricadecodigo.cl>
2022-04-27 20:23:14 -0400

    busqueda

--- lunes25.js
+++ lunes25.js
@@ -1,17 +1,18 @@
-function losAsincronicos()  {
-    console.log("resultado", new Date())
-    const artista = JSON.parse(this.responseText)
-    const img = document.createElement("img")
-    img.src = artista.artists[0].strArtistBanner
-    const imagen = document.getElementById("imagen")
-    imagen.appendChild(img)
-
-
+function desplegarResultado()  {
+    const artista = JSON.parse(this.responseText);
+    const img = document.createElement("img");
+    img.src = artista.artists[0].strArtistThumb;
+    const imagen = document.getElementById("imagen");
+    imagen.innerHTML = "";
+    imagen.appendChild(img);
 }
 
+function buscar()   {
+    const condicion = document.getElementById("condicion").value
+    const request = new XMLHttpRequest();
+    request.addEventListener('load', desplegarResultado);
+    request.open('GET', 'https://www.theaudiodb.com/api/v1/json/2/search.php?s=' + condicion);
+    request.send();
+}
 
-const request = new XMLHttpRequest();
-request.addEventListener('load', losAsincronicos);
-request.open('GET', 'https://www.theaudiodb.com/api/v1/json/2/search.php?s=queen');
-request.send();
 
